ff03f97daa71f6ac3fc3181920434ada5e5ef17b
smalldirtybird <smalldirtybird@gmail.com>
2022-06-15 17:27:02 +0300

    Set DialogFlow

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# digital_secretary
+# Digital smart secretary
+
+This bot will answer frequently asked questions automatically due to neural network learning.
  
+## How to prepare:
+1. Make sure Python installed on your PC - you can get it from [official website](https://www.python.org/).
+   
+
+2. Install libraries with pip:
+    ```
+    pip3 install -r requirements.txt
+   ```
+   
+3. Create a Telegram bot which will answer to questions - just send message `/newbot` to [@BotFather](https://telegram.me/BotFather) and follow instructions.
+    After bot will be created, get token from @BotFather and add to .env file in directory with main.py file(use Notepad++):
+    ```
+    TELEGRAM_BOT_TOKEN ='your_telegram_bot_token'
+    ```
+    Put your token instead of value in quotes.
+
+4. Add to .env file string looks like
+    ```
+   DIALOGFLOW_SESSION_ID = 'session_id'
+   ```
+   where 'session_id' - arbitrary numeric value as which you can use your Telegram user ID. Just write `/start` to [@getmyid_bot](https://telegram.me/getmyid_bot)
+
+
+## How to use:
+Run `main.py` with console. Use `cd` command if you need to change directory:
+```
+D:\>cd D:\learning\python\Chat_bots\devman_review_notifier
+D:\learning\python\Chat_bots\devman_review_notifier>python main.py
+```
--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import logging
 import os
+from pprint import pprint
 
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+        )
+    return response.query_result.fulfillment_text
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # user = update.effective_user
-    await update.message.reply_html(
-        rf'Здравствуйте',
-        reply_markup=ForceReply(selective=True),
-    )
+    await update.message.reply_text(rf'Здравствуйте')
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    intent = detect_intent_text(project_id=df_project_id,
+                                session_id=df_session_id,
+                                text=update.message.text,
+                                language_code='ru_RU')
+    await update.message.reply_text(intent)
 
 
 if __name__ == '__main__':
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
     )
-    logger = logging.getLogger(__name__)# Load env
+    logger = logging.getLogger(__name__)
     load_dotenv()
     tg_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    df_project_id = os.environ['DIALOGFLOW_PROJECT_ID']
+    df_session_id = os.environ['DIALOGFLOW_SESSION_ID']
     application = Application.builder().token(tg_bot_token).build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, answer))
     application.run_polling()
