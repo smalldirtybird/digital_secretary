169b0f757aba6207e9bef4e75d6186a197ba7e77
smalldirtybird <smalldirtybird@gmail.com>
2022-06-16 12:44:46 +0300

    Echo bot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,18 +1,23 @@
 import os
+import random
 import vk_api
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
 if __name__ == '__main__':
     load_dotenv()
     vk_session = vk_api.VkApi(token=os.environ['VK_GROUP_TOKEN'])
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
